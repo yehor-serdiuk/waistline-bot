6edbf06d4d359fb7b13fe885e3ea8b24c7e36c3a
Egor Serdyuk <volcanic-cupcake@proton.me>
2023-06-14 13:27:48 +0300

    initialize project

---
+++ src/main/java/ua/volcaniccupcake/waistline/WaistlineApplication.java
@@ -0,0 +1,13 @@
+package ua.volcaniccupcake.waistline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaistlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaistlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/volcaniccupcake/waistline/WaistlineApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.volcaniccupcake.waistline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaistlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
